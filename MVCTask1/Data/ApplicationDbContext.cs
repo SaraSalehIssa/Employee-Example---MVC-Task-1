3844dc4fb8b60bfcaa50efe8df92573e98855337
SaraIssa <issasalehsara2001@gmail.com>
2024-05-08 00:28:18 +0300

    MVC task 1 solution

---
+++ MVCTask1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTask1.Models;
+
+namespace MVCTask1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=Task1MVC;trusted_connection=true;trustServerCertificate=true");
+        }
+    }
+}
---
+++ MVCTask1/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCTask1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public int Age { get; set; }
+    }
+}
